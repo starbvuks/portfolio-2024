3f6a4f43d5b8511dba6ecce1e56f34daf38e66cb
starbvuks <starbvuks@gmail.com>
2024-05-14 03:26:02 +0530

    iykyk

--- src/app/s3crets/page.js
+++ src/app/s3crets/page.js
@@ -14,8 +14,8 @@ const Secrets = () => {
     <div
       className={
         darkMode
-          ? `text-[#DBDBDB] flex flex-col bg-[#131313] transition-all duration-500`
-          : `text-[#131313] flex flex-col bg-[#DBDBDB] transition-all duration-500`
+          ? `text-[#DBDBDB] h-screen flex flex-col bg-[#131313] transition-all duration-[5000ms]`
+          : `text-[#131313] h-screen flex flex-col bg-[#DBDBDB] transition-all duration-[5000ms]`
       }
     >
       <div
@@ -29,14 +29,14 @@ const Secrets = () => {
           <span
             className={
               darkMode
-                ? "font-gambarino text-4xl md:text-7xl text-[#DBDBDB] hover:text-[#8BCD00] cursor-none tracking-tight transition-all duration-500"
-                : "font-gambarino text-4xl md:text-7xl text-[#323232] hover:text-[#8BCD00] cursor-none tracking-tight transition-all duration-500"
+                ? "font-gambarino text-4xl md:text-7xl text-[#DBDBDB] hover:text-[#8BCD00] cursor-help tracking-tight transition-all duration-500"
+                : "font-gambarino text-4xl md:text-7xl text-[#323232] hover:text-[#8BCD00] cursor-help tracking-tight transition-all duration-500"
             }
           >
-            You Weren&apos;t Supposed To See This 
+            You Weren&apos;t Supposed To See This
           </span>
           <a
-          href="/"
+            href="/"
             className={
               darkMode
                 ? "flex items-center font-gambarino font-light text-sm md:text-lg text-[#323232] hover:text-[#8BCD00] tracking-tight transition-all duration-500"
@@ -67,8 +67,8 @@ const Secrets = () => {
       <div
         className={
           darkMode
-            ? "flex flex-col px-4 md:px-8 py-4 md:py-6 border-b-2 border-[#242424] transition-all duration-500"
-            : "flex flex-col px-4 md:px-8 py-4 md:py-6 border-b-2 border-[#d2d2d2] transition-all duration-500"
+            ? "flex flex-col px-4 md:px-8 py-4 md:py-6 border-b-2 border-[#242424] transition-all duration-500 cursor-none"
+            : "flex flex-col px-4 md:px-8 py-4 md:py-6 border-b-2 border-[#d2d2d2] transition-all duration-500 cursor-none"
         }
       >
         {/* Title */}
@@ -90,16 +90,29 @@ const Secrets = () => {
               : "flex justify-between font-esenka text-sm md:text-3xl text-[#3c3c3c] transition-all duration-500"
           }
         >
-          <div className="flex flex-col gap-1 md:gap-4 font-black py-4 md:py-8 w-full">
-            <div className="flex justify-between w-full">
-              <a>DEXTER WAS GOOD UNTIL S3 & S4 = OVERRATED</a>
-              <span className="font-esenka font-normal text-xs md:text-xl">
+          <div
+            className={
+              darkMode
+                ? "flex flex-col gap-1 md:gap-4 font-black border-l-[1.5px] border-[#3c3c3c] pl-4 py-4 md:my-4 w-full"
+                : "flex flex-col gap-1 md:gap-4 font-black border-l-[1.5px] border-[#aaaaaa] pl-4 py-4 md:my-4 w-full"
+            }
+          >
+            <div className="flex justify-between w-full hover:text-[#8BCD00] cursor-none">
+              <span className="w-[90%]">
+                IT&apos;S FUNNY CAUSE THE PORTFOLIO I SPENT YEARS TRYING TO DO
+                RIGHT IS THE MOST SIMPLE SITE I&apos;VE EVER MADE. IF I HATE
+                THIS IN A MONTH OR TWO ITS BACK TO SQUARE ONE, HOPEFULLY THIS
+                LOOK DOESNT LOOSE IT&apos;S NOVELTY.
+              </span>
+              <span className="font-esenka font-normal text-xs md:text-2xl">
                 1
               </span>
             </div>
-            <div className="flex justify-between w-full">
-              <a>ORANGE LAYS BEST LAYS</a>
-              <span className="font-esenka font-normal text-xs md:text-xl">
+            <div className="flex justify-between w-full hover:text-[#8BCD00] cursor-none">
+              <a href="https://www.fontshare.com/fonts/gambarino" target="_blank" className="w-[90%] cursor-none">
+                THIS FONT GO TOO HARD
+              </a>
+              <span className="font-esenka font-normal text-xs md:text-2xl">
                 2
               </span>
             </div>
